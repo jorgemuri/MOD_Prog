9b77a9b0d08044c1af653126e2bfe194e2c04fcb
Jorge <jmurilloc12@iescastelar.com>
2023-10-18 13:57:41 +0200

    Ejercicios de bucles hechos.

--- EstructuraControl/Bucles/EscaleraAsteristicos.java
+++ EstructuraControl/Bucles/EscaleraAsteristicos.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 public class EscaleraAsteristicos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num;
+        int num,numasteriscos;
 
         System.out.println("Dime un número: ");
         num = sc.nextInt();
-
+        numasteriscos = 1;
         for (int i = 1;i <= num; i++){
-            System.out.print("*");
+            for (int n = 1; n <= numasteriscos; n++){
+                System.out.print("*");
+            }
+            System.out.println("");
+            numasteriscos++;
         }
     }
 }
