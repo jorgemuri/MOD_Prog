514c6340818bf0105aa2bd97a0483dfbc94dbbbe
Jorge <jmurilloc12@iescastelar.com>
2023-11-02 14:14:34 +0100

    Hola

---
+++ Clases/Actividad1_Clases/src/persona/Persona.java
@@ -0,0 +1,16 @@
+package persona;
+
+public class Persona {
+
+    /*Propiedades*/
+    private String nombre = "", apellidos="",dni="";
+    private int anioNacimiento;
+
+    /*Fin propiedades*/
+
+    /*Métodos*/
+
+    public void Persona(String nombre, String apellidos, String dni, int anioNacimiento){}
+
+    /*Fin métodos*/
+}
