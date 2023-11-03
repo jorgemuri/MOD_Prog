25e89f86d06f395a05cc23fef597e88791fdd3a5
Jorge <jmurilloc12@iescastelar.com>
2023-11-03 10:16:21 +0100

    Cambio en el año de nacimiento de persona 2.

--- Clases/Actividad1_Clases/src/principal.java
+++ Clases/Actividad1_Clases/src/principal.java
@@ -4,7 +4,7 @@ public class principal {
     public static void main(String[] args) {
         /*Creo los 2 objetos*/
         Persona p1 = new Persona("Pedro", "Pérez", "24957235X", 1998);
-        Persona p2 = new Persona("Luis", "León","09326485F",1990);
+        Persona p2 = new Persona("Luis", "León","09326485F",2009);
 
         /*Llamo al método toString de cada objeto*/
         System.out.println(p1.toString());
