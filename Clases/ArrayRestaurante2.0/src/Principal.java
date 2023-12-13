4f3f3c751fdc5fbe7e4181e4b40e497c73db09a7
jorgemuri <jmurilloc12@iescastelar.com>
2023-12-13 16:14:05 +0100

    Terminado ArrayRestaurantes2.0

--- Clases/ArrayRestaurante2.0/src/Principal.java
+++ Clases/ArrayRestaurante2.0/src/Principal.java
@@ -8,6 +8,7 @@ public class Principal {
 
         String nombre_restaurante = "";
         int numero_mesas;
+        boolean dentro = true;
 
         // pregunto el número de restaurantes en el que quiere el cliente
         System.out.println("Dime el número de restaurantes: ");
@@ -25,6 +26,54 @@ public class Principal {
             //creo cada objeto en el array
             restaurantes[i] = new Restaurantes(nombre_restaurante, numero_mesas);
         }
+        while (dentro){
+            //pregunto para que restaurante van a hacer la iteración
+            System.out.println("¿Para cual de los restaurantes quieres pedir mesa?");
+            for(int i = 0; i < restaurantes.length; i++){
+                System.out.println((i + 1) + " - " + restaurantes[i].getNombre_restaurante());
+            }
+            //guardo la id del Restaurante
+            System.out.println("(Pon el número)");
+            int idRestaurante = sc.nextInt();
+            idRestaurante = idRestaurante-1;
 
+            //muestro como van las mesas
+            restaurantes[idRestaurante].mostrarmesas();
+
+            //pregunto el número de personas
+            System.out.println("\n¿Cuántas personas sois? (pon -1 para finalizar): ");
+            int n_personas = sc.nextInt();
+            //miro si se quiere salir o no
+            if(n_personas != -1){
+
+                if (n_personas > 4 || n_personas < 0){
+                    System.out.printf("Lo siento, no admitimos grupos de %d, haga grupos de 4 personas como máximo e inténtelo de nuevo", n_personas);
+                }
+                else if(n_personas == 0){
+                    System.out.println("FUERA FANTASMA");
+                }
+                else{
+                    int mesaN = restaurantes[idRestaurante].buscarmesa(n_personas);
+                    if (mesaN == 0){
+                        System.out.println("Lo siento, en estos momentos no queda sitio.");
+                    }
+                    else{
+                        if(mesaN < 100){
+                            System.out.printf("Sientate en la mesa número %d\n\n", mesaN);
+                            restaurantes[idRestaurante].mostrarmesas();
+                        }
+                        else {
+                            mesaN = mesaN - 100;
+                            System.out.printf("Tendrás que compartir mesa, por favor, sientate en la mesa número %d\n\n",mesaN);
+                            restaurantes[idRestaurante].mostrarmesas();
+                        }
+                    }
+                }
+            }
+            else {
+                dentro = false;
+            }
+
+        }
     }
 }
--- Clases/ArrayRestaurante2.0/src/Restaurante/Restaurantes.java
+++ Clases/ArrayRestaurante2.0/src/Restaurante/Restaurantes.java
@@ -4,32 +4,42 @@ public class Restaurantes {
 
     //PROPIEDADES
     private String nombre_restaurante = "";
+    public int[] matriz;
+    public int[] mesas;
+
 
     public Restaurantes(String nombre_restaurante, int n_mesas) {
         //Este es el numero de mesas:
-        int[] matriz = new int[n_mesas];
+        matriz = new int[n_mesas];
         //Relleno la matriz con números aleatorios:
-        int[] mesas = rellenarmatriz(matriz);
+        mesas = rellenarmatriz();
 
         this.nombre_restaurante = nombre_restaurante;
-        mostrarmesas(mesas);
+        mostrarmesas();
     }
 
     //relleno la matriz con el numero de ocupantes aleatorio entre 0 y 4
-    private int[] rellenarmatriz(int[] matriz){
+    private int[] rellenarmatriz(){
         for (int i = 0; i < matriz.length; i++){
             matriz[i] = (int) (Math.random()*4);
         }
         return matriz;
     }
 
-    public void mostrarmesas(int[] mesas){
+    public String getNombre_restaurante() {
+        return nombre_restaurante;
+    }
+
+
+    public void mostrarmesas(){
         System.out.printf("%13s", "NºMesa: ");
         for (int i = 0; i < 2; i++){
             for (int j = 0; j < mesas.length; j++){
                 if (i == 0){
+                    //muestro número de mesa
                     System.out.printf("%2d ", j+1);
                     if(j == (mesas.length -1)){
+                        //muestro número de ocupantes
                         System.out.println();
                         System.out.printf("%13s", "NºOcupantes: ");
                     }
@@ -42,5 +52,22 @@ public class Restaurantes {
                 }
             }
         }
+    }
+
+    public int buscarmesa(int numero_comensales){
+        for (int i = 0; i < mesas.length; i++){
+
+            if(mesas[i] == 0){
+                mesas[i] = mesas[i] + numero_comensales;
+                return i + 1;
+            }
+        }
+        for (int i = 0; i < mesas.length; i++){
+            if(mesas[i] + numero_comensales <= 4){
+                mesas[i] = mesas[i] + numero_comensales;
+                return i + 1 + 100;
+            }
+        }
+        return 0;
     }
 }
