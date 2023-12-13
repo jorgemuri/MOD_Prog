d0d80014575aeac97deb951802f8408879e5209b
Jorge <jmurilloc12@iescastelar.com>
2023-12-13 13:33:42 +0100

    Ejercicio MODIFICADO y BIEN

--- Arrays/Ejercicio4/src/Estatura.java
+++ Arrays/Ejercicio4/src/Estatura.java
@@ -1,12 +1,12 @@
 public class Estatura {
     public static void main(String[] args) {
-        String paises[] = {"España", "Rusia", "Japón", "Australia"};
-        float estaturas[][] = new float[4][10];
-        float media = 0, max = 0, min = 0, aux = 0;
+        String[] paises = {"España", "Rusia", "Japón", "Australia"};
+        float[][] estaturas = new float[4][10];
+        float media, max, min, aux;
 
         /*Cabecera*/
 
-        System.out.printf("\n    Países|");
+        System.out.print("\n    Países|");
         for (int i = 10; i>0; i--){
             System.out.print("       ");
         }
@@ -14,27 +14,26 @@ public class Estatura {
 
         for (int i = 0; i < 4; i++){
             System.out.printf("%10s ",paises[i]);
-            min = 0;
+            min = 1000;
             max = 0;
             media = 0;
+        /*FIN CABECERA*/
             for (int j = 0; j < 10; j++){
                 estaturas[i][j] = (float) (Math.random() * 70) + 140;
+                //almaceno los datos de la fila en la media para después hacerla (dividir entre 10)
                 media = media + estaturas[i][j];
                 aux = estaturas[i][j];
                 if(aux > max){
                     max = aux;
                 }
-                else{
+                if(aux < min){
                     min = aux;
                 }
 
                 System.out.printf("%.2f ", estaturas[i][j]);
             }
             media = media / 10;
-            System.out.printf(" %.0f  %.2f  %.2f", media, max, min);
-            System.out.println("");
+            System.out.printf(" %d  %.2f  %.2f\n", (int)media, max, min);
         }
-
-
     }
 }
