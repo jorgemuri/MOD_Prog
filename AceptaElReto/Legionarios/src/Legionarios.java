db29cb34e851a969c05db4b1843e3250c4cba4bf
jorgemuri <jmurilloc12@iescastelar.com>
2023-12-15 00:17:06 +0100

    Finalizado algoritmo Escudos Romanos

--- AceptaElReto/Legionarios/src/Legionarios.java
+++ AceptaElReto/Legionarios/src/Legionarios.java
@@ -7,30 +7,47 @@ public class Legionarios {
         boolean dentro = true;
 
         while(dentro){
+            boolean dentro2 = true;
             int tamano_cuadrado = 1;
             String datos = "";
             int N_necesario = 0;
             int N_Legionarios = sc.nextInt();
+            int perimetro = 0;
             if(N_Legionarios == 0){
                 dentro = false;
             }
             else{
-                datos = optimizarFormacion(N_Legionarios, tamano_cuadrado);
-                String[] datos_split = datos.split("|");
-                System.out.println(datos_split[1]);
-                tamano_cuadrado = Integer.parseInt(datos_split[0]);
-                N_Legionarios = Integer.parseInt(datos_split[1]);
-                //aqui acumulo los escudos
-                //compruebo si el cuadrado no es de 1x1 para no sumar las esquinas
-                if(tamano_cuadrado == 1){
-                    N_necesario = N_necesario + 1;
+                while (dentro2){
+                    datos = optimizarFormacion(N_Legionarios, tamano_cuadrado);
+                    String[] datos_split = datos.split(" ");
+                    tamano_cuadrado = Integer.parseInt(datos_split[0]);
+                    N_Legionarios = Integer.parseInt(datos_split[1]);
+                    //aqui acumulo los escudos
+                    //compruebo si el cuadrado no es de 1x1
+                    if(tamano_cuadrado == 1){
+                        N_necesario = N_necesario + 5;
+                    }
+                    else {
+                        //laterales
+                        if(tamano_cuadrado == 2){
+                            N_necesario = (N_necesario + (4*3));
+                        }
+                        else {
+                            //sumo todos los laterales
+                            perimetro = tamano_cuadrado*4 - 4;
+                            // le sumo 12 por las esquinas
+                            N_necesario = N_necesario + ((perimetro-4)*2) + 12;
+                        }
+                        //sumo el interior
+                        int interior = tamano_cuadrado*tamano_cuadrado - (tamano_cuadrado*4-4);
+                        N_necesario = N_necesario + interior;
+                    }
+                    if(N_Legionarios == 0){
+                        dentro2 = false;
+                    }
+                    tamano_cuadrado = 1;
                 }
-                else {
-                    N_necesario = N_necesario + (tamano_cuadrado*tamano_cuadrado + 4);
-                }
-                if(N_Legionarios == 0){
-                    System.out.println(N_necesario);
-                }
+                System.out.println(N_necesario);
             }
         }
     }
@@ -38,8 +55,7 @@ public class Legionarios {
         String datos = "";
         if (tamano_cuadrado * tamano_cuadrado > N_Legionarios) {
             N_Legionarios = N_Legionarios - ((tamano_cuadrado - 1)*(tamano_cuadrado-1));
-            datos=(""+(tamano_cuadrado - 1) + "|" + N_Legionarios);
-            System.out.println(datos);
+            datos=(""+(tamano_cuadrado - 1) + " " + N_Legionarios);
             return datos;
         }
         else {
