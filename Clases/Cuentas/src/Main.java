a914ec916096e1ec428bdd600443c3e344b782f4
Jorge <jmurilloc12@iescastelar.com>
2024-01-09 12:14:22 +0100

    Fallo en la morosidad resuelto

--- Clases/Cuentas/src/Main.java
+++ Clases/Cuentas/src/Main.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String comandos = "";
-        boolean salir = false, NcuentaExiste = false;
+        String comandos;
+        boolean salir = false, NcuentaExiste;
         //creo un array de objetos
         Cuenta[] cuentas = new Cuenta[10];
         int[] arrayNcuentas = new int[10];
         int N_Cuenta = -1;
         int posicionCuenta = 0;
-        float saldo = 0;
+        float saldo;
 
         while (!salir){
             NcuentaExiste = false;
@@ -25,96 +25,107 @@ public class Main {
             } else if (comandos.equals("EXIT")) {
                 salir = true;
             } else{
-                if(comandos.equals("AC")){
-                    N_Cuenta++; //indice que va a ir en el array de objetos
-                    //le pido el número de cuenta
-                    System.out.println("Dime el número de la cuenta: ");
-                    int NumeroCuenta = sc.nextInt();
-                    //compruebo si el número de cuenta ya existe
-                    for(int i = 0; i < arrayNcuentas.length;i++){
-                        if(NumeroCuenta == arrayNcuentas[i]){
-                            NcuentaExiste = true;
-                        }
-                    }
-                    if(!NcuentaExiste){
-                        if(posicionCuenta > 9){
-                            //supera el límite del array
-                            System.out.println("No se pueden crear más cuentas");
-                        }
-                        else{
-                            arrayNcuentas[posicionCuenta] = NumeroCuenta;
-                            posicionCuenta++;
-                        }
-                    }
-                    cuentas[N_Cuenta] = new Cuenta(NumeroCuenta); //sin saldo
+                switch (comandos) {
+                    case "AC" -> {
+                        N_Cuenta++; //indice que va a ir en el array de objetos
 
-                } else if (comandos.equals("ACS")) {
-                    N_Cuenta++; //indice que va a ir en el array de objetos
-                    //le pido el número de cuenta
-                    System.out.println("Dime el número de la cuenta: ");
-                    int NumeroCuenta = sc.nextInt();
-                    //compruebo si el número de cuenta ya existe
-                    for(int i = 0; i < arrayNcuentas.length;i++){
-                        if(NumeroCuenta == arrayNcuentas[i]){
-                           NcuentaExiste = true;
+                        //le pido el número de cuenta
+                        System.out.println("Dime el número de la cuenta: ");
+                        int NumeroCuenta = sc.nextInt();
+                        //compruebo si el número de cuenta ya existe
+                        for (int i = 0; i < arrayNcuentas.length; i++) {
+                            if (NumeroCuenta == arrayNcuentas[i]) {
+                                NcuentaExiste = true;
+                            }
+                        }
+                        if (!NcuentaExiste) {
+                            if (posicionCuenta > 9) {
+                                //supera el límite del array
+                                System.out.println("No se pueden crear más cuentas");
+                            } else {
+                                arrayNcuentas[posicionCuenta] = NumeroCuenta;
+                                posicionCuenta++;
+                            }
+                        }
+                        cuentas[N_Cuenta] = new Cuenta(NumeroCuenta); //sin saldo
+
+
+                    }
+                    case "ACS" -> {
+                        N_Cuenta++; //indice que va a ir en el array de objetos
+
+                        //le pido el número de cuenta
+                        System.out.println("Dime el número de la cuenta: ");
+                        int NumeroCuenta = sc.nextInt();
+                        //compruebo si el número de cuenta ya existe
+                        for (int i = 0; i < arrayNcuentas.length; i++) {
+                            if (NumeroCuenta == arrayNcuentas[i]) {
+                                NcuentaExiste = true;
+                            }
+                        }
+                        if (!NcuentaExiste) {
+                            if (posicionCuenta > 10) {
+                                //supera el límite del array
+                                System.out.println("No se pueden crear más cuentas");
+                            } else {
+                                arrayNcuentas[posicionCuenta] = NumeroCuenta;
+                                posicionCuenta++;
+                            }
+                        }
+                        //pregunto el saldo
+                        System.out.print("Dime el saldo de la cuenta: ");
+                        saldo = sc.nextFloat();
+                        cuentas[N_Cuenta] = new Cuenta(NumeroCuenta, saldo); //con saldo
+
+                    }
+                    case "CS" -> {
+                        System.out.printf("Actualmente estás en la cuenta con numero de cuenta %d\n", cuentas[N_Cuenta].getNCuenta());
+                        System.out.printf("Tu saldo es %.2f\n", cuentas[N_Cuenta].getSaldo());
+                    }
+                    case "ID" -> {
+                        System.out.print("Dime el dinero a ingresar: ");
+                        float importe = sc.nextFloat();
+                        cuentas[N_Cuenta].Ingresar(importe);
+                    }
+                    case "RD" -> {
+                        System.out.print("Dime el dinero que quieres retirar: ");
+                        float importe = sc.nextFloat();
+                        cuentas[N_Cuenta].Retirar(importe);
+                    }
+                    case "CM" -> {
+                        //consultar morosidad
+                        if (cuentas[N_Cuenta].esMorosa()) {
+                            System.out.println("Esta cuenta es morosa");
+                        } else {
+                            System.out.println("Esta cuenta no es morosa");
                         }
                     }
-                    if(!NcuentaExiste){
-                        if(posicionCuenta > 10){
-                            //supera el límite del array
-                            System.out.println("No se pueden crear más cuentas");
+                    case "NCS" -> System.out.printf("La cuenta seleccionada es: %d\n", cuentas[N_Cuenta].getNCuenta());
+                    case "SC" -> {
+                        System.out.println("Estas son tus cuentas: ");
+                        for (int i = 0; i < cuentas.length; i++) { //les muestro las cuentas existentes
+                            if (cuentas[i] != null) {
+                                System.out.printf("%d. %d\n", i, cuentas[i].getNCuenta());
+                            }
                         }
-                        else{
-                            arrayNcuentas[posicionCuenta] = NumeroCuenta;
-                            posicionCuenta++;
-                        }
+                        System.out.print("Pon el índice para seleccionar la cuenta: ");
+                        N_Cuenta = sc.nextInt();
                     }
-                    //pregunto el saldo
-                    System.out.print("Dime el saldo de la cuenta: ");
-                    saldo = sc.nextFloat();
-                    cuentas[N_Cuenta] = new Cuenta(NumeroCuenta, saldo); //con saldo
-                } else if (comandos.equals("CS")) {
-                    System.out.printf("Actualmente estás en la cuenta con numero de cuenta %d\n",cuentas[N_Cuenta].getNCuenta());
-                    System.out.printf("Tu saldo es %.2f\n",cuentas[N_Cuenta].getSaldo());
-                } else if (comandos.equals("ID")) {
-                    System.out.print("Dime el dinero a ingresar: ");
-                    float importe = sc.nextFloat();
-                    cuentas[N_Cuenta].Ingresar(importe);
-                } else if (comandos.equals("RD")) {
-                    System.out.print("Dime el dinero que quieres retirar: ");
-                    float importe = sc.nextFloat();
-                    cuentas[N_Cuenta].Retirar(importe);
-                } else if (comandos.equals("CM")) {
-                    //consultar morosidad
-                    if(cuentas[N_Cuenta].esMorosa()){
-                        System.out.println("Esta cuenta es morosa");
-                    }
-                } else if (comandos.equals("NCS")) {
-                    System.out.printf("La cuenta seleccionada es: %d\n", cuentas[N_Cuenta].getNCuenta());
-                } else if (comandos.equals("SC")) {
-                    System.out.println("Estas son tus cuentas: ");
-                    for (int i = 0; i < cuentas.length; i++){ //les muestro las cuentas existentes
-                        if(cuentas[i] != null){
-                            System.out.printf("%d. %d\n",i, cuentas[i].getNCuenta());
-                        }
-                    }
-                    System.out.print("Pon el índice para seleccionar la cuenta: ");
-                    N_Cuenta = sc.nextInt();
-                } else {
-                    System.out.println("Ese comando no existe. TIP: Escribe en mayúsculas");
+                    default -> System.out.println("Ese comando no existe. TIP: Escribe en mayúsculas");
                 }
             }
         }
     }
     private static void mostrarComandos(){
-        System.out.println("ABRIR CUENTA: AC" +
-                "\nABRIR CUENTA CON SALDO: ACS" +
-                "\nCONSULTAR SALDO: CS" +
-                "\nINGRESAR DINERO: ID" +
-                "\nRETIRAR DINERO: RD" +
-                "\nCONSULTAR MOROSIDAD: CM" +
-                "\nNÚMERO DE LA CUENTA SELECCIONADA: NCS" +
-                "\nSELECCIONAR CUENTA: SC" +
-                "\nSALIR: EXIT");
+        System.out.println("""
+                ABRIR CUENTA: AC
+                ABRIR CUENTA CON SALDO: ACS
+                CONSULTAR SALDO: CS
+                INGRESAR DINERO: ID
+                RETIRAR DINERO: RD
+                CONSULTAR MOROSIDAD: CM
+                NÚMERO DE LA CUENTA SELECCIONADA: NCS
+                SELECCIONAR CUENTA: SC
+                SALIR: EXIT""");
     }
 }
